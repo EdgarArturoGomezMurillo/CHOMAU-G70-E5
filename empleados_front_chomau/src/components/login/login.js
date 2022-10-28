d0f99ac9f79c7f6e4e305817b8574d6ad9fdb295
azhtiray <55214210+azhtiray@users.noreply.github.com>
2022-10-27 21:11:23 -0500

    modificado 27

--- empleados_front_chomau/src/components/login/login.js
+++ empleados_front_chomau/src/components/login/login.js
@@ -1,6 +1,7 @@
-import App from '../../App';
 import React from "react";
-import {Container, Button, Form} from "react-bootstrap";
+import { Container, Button, Form, Row } from "react-bootstrap";
+
+
 export default class login extends React.Component {
     constructor(props) {
         super(props);
@@ -8,28 +9,38 @@ export default class login extends React.Component {
     state = {};
     render() {
         return (
-            <Container id ="login-container" style = {{ marginTop: 200}}>
-                <Form>
-                    <Form.Group>
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
+            <Container id="login-container" style={{ marginTop: 200 }}>
+                <Row>
 
-                    <Form.Group>
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
+                    <Col>
+                        <Row>                        
+                            <h2>
+                                Iniciar Sesion
+                            </h2>
 
-                    <Form.Group>
-                        <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                </Form>
+                            <Form>
+                                <Form.Group>
+                                    <Form.Label style={{ float: "left" }}>Email address</Form.Label>
+                                    <Form.Control type="email" placeholder="Enter email" />
+
+                                </Form.Group>
+
+                                <Form.Group>
+                                    <Form.Label style={{ float: 'left' }}>Password</Form.Label>
+                                    <Form.Control type="password" placeholder="Password" />
+                                </Form.Group>
+
+
+                                <Button variant="primary"
+
+                                    style={{ marginTop: 20, width: "100 %" }}
+                                >
+                                    Iniciar Sesion
+                                </Button>
+                            </Form>
+                        </Row>
+                    </Col>
+                </Row>
             </Container>
         );
     }
