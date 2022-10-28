e380fa79d16b0b637b4ba205c116c1d16c40a9fe
azhtiray <55214210+azhtiray@users.noreply.github.com>
2022-10-28 14:01:47 -0500

    Update login.js

--- empleados_front_chomau/src/components/login/login.js
+++ empleados_front_chomau/src/components/login/login.js
@@ -69,15 +69,3 @@ iniciarSesion(){
     );
   }
 }
-
-// class  extends Component {
-//     constructor(props) {
-//         super(props);
-//     }
-//     state = {  }
-//     render() {
-//         return (  );
-//     }
-// }
-
-// export default ;
