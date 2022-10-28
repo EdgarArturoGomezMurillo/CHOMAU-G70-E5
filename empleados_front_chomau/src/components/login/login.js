cca4737cc0dca1d0f5e20a00b117dac7bb2bcbd8
Yarithza <azhtiray@gmail.com>
2022-10-27 21:36:00 -0500

    modificado 27

---
+++ empleados_front_chomau/src/components/login/login.js
@@ -0,0 +1,12 @@
+import React from "react";
+class  extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default ;
